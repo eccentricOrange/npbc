42a705223d27e43cd38b737c8455b3f12a7def4f
Anuj Verma <anuj.verma20132@gmail.com>
2021-10-22 02:12:17 +0530

    ?

--- install_run/install.sh
+++ install_run/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/eccentricOrange/npbc.git /usr/local/npbc
-cp -r /usr/local/npbc/data ~/.npbc/
-echo 'alias npbc="cd /usr/local/npbc && code/main.py"' >> ~/.bashrc
+git clone https://github.com/eccentricOrange/npbc.git ~/bin/npbc
+cp -r ~/bin/npbc/data ~/.npbc/
+echo 'alias npbc="cd ~/bin/npbc && python code/main.py"' >> ~/.bashrc
+alias npbc="cd ~/bin/npbc && python code/main.py"
