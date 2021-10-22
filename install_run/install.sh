cfcb58edd5670bac00d1290d19cc14cb207705bb
Anuj Verma <anuj.verma20132@gmail.com>
2021-10-22 16:02:27 +0530

    🤦‍♂️

--- install_run/install.sh
+++ install_run/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-al = 'alias npbc="pyenv shell 3.9.7/envs/npbc-env && python ~/bin/npbc/code/main.py"'
+al='alias npbc="pyenv shell 3.9.7/envs/npbc-env && python ~/bin/npbc/code/main.py"'
 
 git clone https://github.com/eccentricOrange/npbc.git ~/bin/npbc
 cp -r ~/bin/npbc/data ~/.npbc/
