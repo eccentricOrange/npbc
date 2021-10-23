98b4bb4d3de9a0a6d3003990ada77f0fc7f41351
Anuj Verma <anuj.verma20132@gmail.com>
2021-10-24 00:20:18 +0530

    ?

--- install.bat
+++ install.bat
@@ -3,7 +3,8 @@ git clone https://github.com/eccentricOrange/npbc.git "%userprofile%\npbc"
 if exist "%userprofile%\.npbc" (
     echo "Not updating user directory."
 ) else (
-    xcopy /s "%userprofile%\npbc\data %userprofile%\.npbc"
+    mkdir "%userprofile%\.npbc"
+    xcopy /s "%userprofile%\npbc\data\ %userprofile%\.npbc\"
 )
 
 setx path "%PATH%;%userprofile%\npbc\bin"
