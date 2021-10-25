84460971052bfa4826470fa6c087c17909c875fa
Anuj Verma <anuj.verma20132@gmail.com>
2021-10-26 02:01:01 +0530

    windows test 3

--- compile.sh
+++ compile.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 pyinstaller -F --add-data includes/undelivered_help.pdf:includes --distpath bin npbc.py
+git add .
+git commit -m "$1"
+git push origin master
+npbc update
--- picompile.sh
+++ picompile.sh
@@ -1,1 +1,5 @@
 pyinstaller -F --add-data includes/undelivered_help.pdf:includes -n pinpbc --distpath bin npbc.py
+git add .
+git commit -m "$1"
+git push origin master
+npbc update
