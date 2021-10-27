574549612269e2b5c804bba83242f0a76eda498d
Anuj Verma <anuj.verma20132@gmail.com>
2021-10-28 00:56:37 +0530

    bugfix?

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ dir=~/.npbc
 if [ ! -d "$dir" ]; then
     cp -r ~/bin/npbc/data ~/.npbc/
 fi
-echo 'alias npbc=sh ~/bin/npbc/bin/npbc.sh' >> ~/.bashrc
-alias npbc=sh ~/bin/npbc/bin/npbc.sh
+chmod +x ~/bin/npbc/bin/npbc.sh
+echo 'alias npbc=~/bin/npbc/bin/npbc.sh' >> ~/.bashrc
+alias npbc=~/bin/npbc/bin/npbc.sh
