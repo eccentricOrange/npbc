8653622a1bbdf613491b8113c14035797b6f3e56
Anuj Verma <anuj.verma20132@gmail.com>
2021-11-02 20:04:34 +0530

    compile scripts pull

--- compile/compile.bat
+++ compile/compile.bat
@@ -2,4 +2,5 @@ git pull
 pyinstaller -F --add-data includes\undelivered_help.pdf;includes --distpath bin/executables npbc.py
 git add .
 git commit -m "windows compile"
+git pull
 git push origin master
--- compile/picompile.sh
+++ compile/picompile.sh
@@ -3,4 +3,5 @@ git pull
 pyinstaller -F --add-data includes/undelivered_help.pdf:includes -n pinpbc --distpath bin/executables npbc.py
 git add .
 git commit -m "linux on arm compile"
+git pull
 git push origin master
