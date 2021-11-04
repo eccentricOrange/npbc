7ca95b7a01fa7a33231a8581af7564fb2a6f770a
Anuj Verma <anuj.verma20132@gmail.com>
2021-11-04 14:13:33 +0530

    windows compile

--- compile/compile.bat
+++
@@ -1,6 +0,0 @@
-git pull
-pyinstaller -F --add-data includes\undelivered_help.pdf;includes --distpath bin/executables npbc.py
-git add .
-git commit -m "windows compile"
-git pull
-git push origin master
---
+++ compile/win-compile.sh
@@ -0,0 +1,6 @@
+git pull
+pyinstaller -F --add-data includes/undelivered_help.pdf;includes --distpath bin/executables npbc.py
+git add .
+git commit -m "windows compile"
+git pull
+git push origin master
