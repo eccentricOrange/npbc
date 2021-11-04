20cffd3c7f6c7d152de1fbfa9861438915823e13
Anuj Verma <anuj.verma20132@gmail.com>
2021-11-04 15:57:37 +0530

    revert

--- compile/win-compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull
-pyinstaller -F --add-data includes/undelivered_help.pdf;includes --distpath bin/executables npbc.py
-git add .
-git commit -m "windows compile"
-git pull
-git push origin master
