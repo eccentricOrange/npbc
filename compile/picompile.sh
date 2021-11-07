2affd4f2f9747b4c043956cb7d99923a9518c588
Anuj Verma <anuj.verma20132@gmail.com>
2021-11-07 14:09:19 +0530

    specpath

--- compile/picompile.sh
+++ compile/picompile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-pyinstaller -F --add-data includes/undelivered_help.pdf:includes -n pinpbc --distpath bin/executables npbc.py
+pyinstaller -F --add-data includes/undelivered_help.pdf:includes -n pinpbc --distpath bin/executables npbc.py  --specpath spec
 git add .
 git commit -m "linux on arm compile"
 git pull
