a5dce5f47a7734a4880bc91f652dbf71a0db615b
Anuj Verma <anuj.verma20132@gmail.com>
2021-11-07 23:27:23 +0530

    renaming

--- compile/compile.bat
+++ compile/compile.bat
@@ -1,5 +1,5 @@
 git pull
-pyinstaller -F --add-data includes\undelivered_help.pdf;includes -nnpbc --distpath bin/executables npbc.py  --specpath spec
+pyinstaller -F --add-data includes\undelivered_help.pdf;includes -n npbc --distpath bin/executables npbc.py  --specpath spec
 git add .
 git commit -m "windows compile"
 git pull
--- compile/compile.sh
+++ compile/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-pyinstaller -F --add-data includes/undelivered_help.pdf:includes -n linnpbc --distpath bin/executables npbc.py --specpath spec
+pyinstaller -F --add-data includes/undelivered_help.pdf:includes -n nnpbc --distpath bin/executables npbc.py --specpath spec
 git add .
 git commit -m "linux on x64 compile"
 git pull
