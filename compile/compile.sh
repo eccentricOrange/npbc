b4e9a170baceba47f88fcb3538cf15a9de1411d8
Anuj Verma <anuj.verma20132@gmail.com>
2022-01-26 21:02:35 +0530

    done desigining. figuring installation

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 git pull
-pyinstaller -F -n npbc_api --distpath bin/executables npbc_api.py --specpath spec
-pyinstaller -F -n npbc_cli --distpath bin/executables npbc_cli.py --specpath spec
+python -m nuitka --include-data-file=/home/anujv/pts/npbc/data/schema.sql=schema.sql --onefile --standalone -o bin/executables/npbc_cli npbc_cli.py
+python -m nuitka --include-data-file=/home/anujv/pts/npbc/data/schema.sql=schema.sql --onefile --standalone -o bin/executables/npbc_api npbc_api.py
+# pyinstaller -F -n npbc_api --distpath bin/executables npbc_api.py --specpath spec
+# pyinstaller -F -n npbc_cli --distpath bin/executables npbc_cli.py --specpath spec
 git add .
 git commit -m "linux on x64 compile"
 git pull
