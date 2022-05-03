60c409714c20e570db6ab78feb515d3b1b2b1ea3
Anuj Verma <anuj.verma20132@gmail.com>
2022-05-03 16:59:35 +0530

    inline foriegn keys

--- data/schema.sql
+++ data/schema.sql
@@ -6,54 +6,46 @@ CREATE TABLE IF NOT EXISTS papers (
 
 CREATE TABLE IF NOT EXISTS papers_days (
     paper_day_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    paper_id INTEGER NOT NULL,
-    day_id INTEGER NOT NULL,
-    FOREIGN KEY (paper_id) REFERENCES papers(paper_id),
-    CONSTRAINT unique_paper_day UNIQUE (paper_id, day_id)
+    paper_id INTEGER NOT NULL REFERENCES papers(paper_id),
+    day_id INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS papers_days_delivered (
     papers_days_delivered_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    paper_day_id INTEGER NOT NULL,
-    delivered INTEGER NOT NULL CHECK (delivered IN (0, 1)),
-    FOREIGN KEY (paper_day_id) REFERENCES papers_days(paper_day_id)
+    paper_day_id INTEGER NOT NULL REFERENCES papers_days(paper_day_id),
+    delivered INTEGER NOT NULL CHECK (delivered IN (0, 1))
 );
 
 CREATE TABLE IF NOT EXISTS papers_days_cost (
     papers_days_cost_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    paper_day_id INTEGER NOT NULL,
-    cost INTEGER,
-    FOREIGN KEY (paper_day_id) REFERENCES papers_days(paper_day_id)
+    paper_day_id INTEGER NOT NULL REFERENCES papers_days(paper_day_id),
+    cost INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS undelivered_strings (
     string_id INTEGER PRIMARY KEY AUTOINCREMENT,
     year INTEGER NOT NULL CHECK (year >= 0),
     month INTEGER NOT NULL CHECK (month >= 0 AND month <= 12),
-    paper_id INTEGER NOT NULL,
-    string TEXT NOT NULL,
-    FOREIGN KEY (paper_id) REFERENCES papers(paper_id)
+    paper_id INTEGER NOT NULL REFERENCES papers(paper_id),
+    string TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS logs (
     log_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    paper_id INTEGER NOT NULL,
+    paper_id INTEGER NOT NULL REFERENCES papers(paper_id),
     entry_timestamp TEXT NOT NULL,
     month INTEGER NOT NULL CHECK (month >= 0 AND month <= 12),
-    year INTEGER NOT NULL CHECK (year >= 0),
-    FOREIGN KEY (paper_id) REFERENCES papers(paper_id)
+    year INTEGER NOT NULL CHECK (year >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS undelivered_dates_logs (
     undelivered_dates_log_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    log_id INTEGER NOT NULL,
-    date_not_delivered TEXT NOT NULL,
-    FOREIGN KEY (log_id) REFERENCES logs(log_id)
+    log_id INTEGER NOT NULL REFERENCES logs(log_id),
+    date_not_delivered TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cost_logs (
     cost_log_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    log_id INTEGER NOT NULL,
-    cost INTEGER NOT NULL,
-    FOREIGN KEY (log_id) REFERENCES logs(log_id)
+    log_id INTEGER NOT NULL REFERENCES logs(log_id),
+    cost INTEGER NOT NULL
 );
