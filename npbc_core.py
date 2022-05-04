2ffd7915b981d554cbed019b402c297493bcc2b2
Anuj Verma <anuj.verma20132@gmail.com>
2022-05-05 03:26:11 +0530

    finish parse rewrite

--- npbc_core.py
+++ npbc_core.py
@@ -1,7 +1,9 @@
+from datetime import date as date_type, datetime, timedelta
 from pathlib import Path
-from calendar import day_name as weekday_names_iterable, monthcalendar
+from calendar import day_name as weekday_names_iterable, monthcalendar, monthrange
 from sqlite3 import connect
 from typing import Generator
+import npbc_regex
 
 
 ## paths for the folder containing schema and database files
@@ -19,6 +21,134 @@ SCHEMA_PATH = Path(__file__).parent / 'schema.sql'  # normal use path
 
 ## list constant for names of weekdays
 WEEKDAY_NAMES = list(weekday_names_iterable)
+
+
+## validate a string that specifies when a given paper was not delivered
+# first check to see that it meets the comma-separated requirements
+# then check against each of the other acceptable patterns in the regex dictionary
+def validate_undelivered_string(*strings: str) -> bool:
+    # check that the string matches one of the acceptable patterns
+    for string in strings:
+        if string and not (
+            npbc_regex.NUMBER_MATCH_REGEX.match(string) or
+            npbc_regex.RANGE_MATCH_REGEX.match(string) or
+            npbc_regex.DAYS_MATCH_REGEX.match(string) or
+            npbc_regex.N_DAY_MATCH_REGEX.match(string) or
+            npbc_regex.ALL_MATCH_REGEX.match(string)
+        ):
+
+            return False
+
+    # if we get here, all strings passed the regex check
+    return True
+
+## if the date is simply a number, it's a single day. so we just identify that date
+def extract_number(string: str, month: int, year: int) -> date_type | None:
+    date = int(string)
+
+    if date > 0 and date <= monthrange(year, month)[1]:
+        return date_type(year, month, date)
+
+
+## if the date is a range of numbers, it's a range of days. we identify all the dates in that range, bounds inclusive
+def extract_range(string: str, month: int, year: int) -> Generator[date_type, None, None]:
+    start, end = [int(date) for date in npbc_regex.HYPHEN_SPLIT_REGEX.split(string)]
+
+    if 0 < start <= end <= monthrange(year, month)[1]:
+        for date in range(start, end + 1):
+            yield date_type(year, month, date)
+
+
+## if the date is the plural of a weekday name, we identify all dates in that month which are the given weekday
+def extract_weekday(string: str, month: int, year: int) -> Generator[date_type, None, None]:
+    weekday = WEEKDAY_NAMES.index(string.capitalize().rstrip('s'))
+
+    for day in range(1, monthrange(year, month)[1] + 1):
+        if date_type(year, month, day).weekday() == weekday:
+            yield date_type(year, month, day)
+
+
+## if the date is a number and a weekday name (singular), we identify the date that is the nth occurrence of the given weekday in the month
+def extract_nth_weekday(string: str, month: int, year: int) -> date_type | None:
+    n, weekday_name = npbc_regex.HYPHEN_SPLIT_REGEX.split(string)
+
+    n = int(n)
+
+    if n > 0 and n <= list(get_number_of_each_weekday(month, year))[WEEKDAY_NAMES.index(weekday_name.capitalize())]:
+        weekday = WEEKDAY_NAMES.index(weekday_name.capitalize())
+
+        valid_dates = [
+            date_type(year, month, day)
+            for day in range(1, monthrange(year, month)[1] + 1)
+            if date_type(year, month, day).weekday() == weekday
+        ]
+
+        return valid_dates[n - 1]
+
+
+## if the text is "all", we identify all the dates in the month
+def extract_all(month: int, year: int) -> Generator[date_type, None, None]:
+    for day in range(1, monthrange(year, month)[1] + 1):
+        yield date_type(year, month, day)
+
+
+## parse a section of the strings
+    # each section is a string that specifies a set of dates
+    # this function will return a set of dates that uniquely identifies each date mentioned across the string
+def parse_undelivered_string(month: int, year: int, string: str) -> set[date_type]:
+    # initialize the set of dates
+    dates = set()
+
+    # check for each of the patterns
+    if npbc_regex.NUMBER_MATCH_REGEX.match(string):
+        number_date = extract_number(string, month, year)
+
+        if number_date:
+            dates.add(number_date)
+
+    elif npbc_regex.RANGE_MATCH_REGEX.match(string):
+        dates.update(extract_range(string, month, year))
+
+    elif npbc_regex.DAYS_MATCH_REGEX.match(string):
+        dates.update(extract_weekday(string, month, year))
+
+    elif npbc_regex.N_DAY_MATCH_REGEX.match(string):
+        n_day_date = extract_nth_weekday(string, month, year)
+
+        if n_day_date:
+            dates.add(n_day_date)
+
+    elif npbc_regex.ALL_MATCH_REGEX.match(string):
+        dates.update(extract_all(month, year))
+
+    else:
+        raise ValueError
+
+    return dates
+
+    
+## parse a string that specifies when a given paper was not delivered
+ # each section states some set of dates
+ # this function will return a set of dates that uniquely identifies each date mentioned across all the strings
+def parse_undelivered_strings(month: int, year: int, *strings: str) -> set[date_type]:
+    # initialize the set of dates
+    dates = set()
+
+    # check for each of the patterns
+    for string in strings:
+        try:
+            dates.update(parse_undelivered_string(month, year, string))
+
+        except ValueError:
+            print(
+                f"""Congratulations! You broke the program!
+                You managed to write a string that the program considers valid, but isn't actually.
+                Please report it to the developer.
+                \nThe string you wrote was: {string}
+                This data has not been counted."""
+            )
+
+    return dates
 
 
 ## ensure DB exists and it's set up with the schema
@@ -47,5 +177,3 @@ def get_number_of_each_weekday(month: int, year: int) -> Generator[int, None, No
             number_of_weekday -= 1
 
         yield number_of_weekday
-
-
--- test_core.py
+++ test_core.py
@@ -1,8 +1,171 @@
-from npbc_core import get_number_of_each_weekday
+from datetime import date as date_type
+import npbc_core
 
 def test_get_number_of_each_weekday():
-    assert list(get_number_of_each_weekday(1, 2022)) == [5, 4, 4, 4, 4, 5, 5]
-    assert list(get_number_of_each_weekday(2, 2022)) == [4, 4, 4, 4, 4, 4, 4]
-    assert list(get_number_of_each_weekday(3, 2022)) == [4, 5, 5 ,5, 4, 4, 4]
-    assert list(get_number_of_each_weekday(2, 2020)) == [4, 4, 4, 4, 4, 5, 4]
-    assert list(get_number_of_each_weekday(12, 1954)) == [4, 4, 5, 5, 5, 4, 4]
+    test_function = npbc_core.get_number_of_each_weekday
+
+    assert list(test_function(1, 2022)) == [5, 4, 4, 4, 4, 5, 5]
+    assert list(test_function(2, 2022)) == [4, 4, 4, 4, 4, 4, 4]
+    assert list(test_function(3, 2022)) == [4, 5, 5 ,5, 4, 4, 4]
+    assert list(test_function(2, 2020)) == [4, 4, 4, 4, 4, 5, 4]
+    assert list(test_function(12, 1954)) == [4, 4, 5, 5, 5, 4, 4]
+
+
+def test_validate_undelivered_string():
+    test_function = npbc_core.validate_undelivered_string
+
+    assert test_function("")
+    assert not test_function("a")
+    assert not test_function("monday")
+    assert not test_function("1-mondays")
+    assert not test_function("1monday")
+    assert not test_function("1 monday")
+    assert not test_function("monday-1")
+    assert not test_function("monday-1")
+    assert test_function("1")
+    assert test_function("6")
+    assert test_function("31")
+    assert test_function("31","")
+    assert test_function("3","1")
+    assert test_function("3","1","")
+    assert test_function("3","1")
+    assert test_function("3","1")
+    assert test_function("3","1")
+    assert test_function("1","2","3-9")
+    assert test_function("1","2","3-9","11","12","13-19")
+    assert test_function("1","2","3-9","11","12","13-19","21","22","23-29")
+    assert test_function("1","2","3-9","11","12","13-19","21","22","23-29","31")
+    assert test_function("1","2","3","4","5","6","7","8","9")
+    assert test_function("mondays")
+    assert test_function("mondays,tuesdays")
+    assert test_function("mondays","tuesdays","wednesdays")
+    assert test_function("mondays","5-21")
+    assert test_function("mondays","5-21","tuesdays","5-21")
+    assert test_function("1-monday")
+    assert test_function("2-monday")
+    assert test_function("all")
+    assert test_function("All")
+    assert test_function("aLl")
+    assert test_function("alL")
+    assert test_function("aLL")
+    assert test_function("ALL")
+
+
+def test_undelivered_string_parsing():
+    MONTH = 5
+    YEAR = 2017
+    test_function = npbc_core.parse_undelivered_strings
+
+
+    assert test_function(MONTH, YEAR, '') == set([])
+
+    assert test_function(MONTH, YEAR, '1') == set([
+        date_type(year=YEAR, month=MONTH, day=1)
+    ])
+
+    assert test_function(MONTH, YEAR, '1-2') == set([
+        date_type(year=YEAR, month=MONTH, day=1),
+        date_type(year=YEAR, month=MONTH, day=2)
+    ])
+
+    assert test_function(MONTH, YEAR, '5-17') == set([
+        date_type(year=YEAR, month=MONTH, day=5),
+        date_type(year=YEAR, month=MONTH, day=6),
+        date_type(year=YEAR, month=MONTH, day=7),
+        date_type(year=YEAR, month=MONTH, day=8),
+        date_type(year=YEAR, month=MONTH, day=9),
+        date_type(year=YEAR, month=MONTH, day=10),
+        date_type(year=YEAR, month=MONTH, day=11),
+        date_type(year=YEAR, month=MONTH, day=12),
+        date_type(year=YEAR, month=MONTH, day=13),
+        date_type(year=YEAR, month=MONTH, day=14),
+        date_type(year=YEAR, month=MONTH, day=15),
+        date_type(year=YEAR, month=MONTH, day=16),
+        date_type(year=YEAR, month=MONTH, day=17)
+    ])
+
+    assert test_function(MONTH, YEAR, '5-17', '19') == set([
+        date_type(year=YEAR, month=MONTH, day=5),
+        date_type(year=YEAR, month=MONTH, day=6),
+        date_type(year=YEAR, month=MONTH, day=7),
+        date_type(year=YEAR, month=MONTH, day=8),
+        date_type(year=YEAR, month=MONTH, day=9),
+        date_type(year=YEAR, month=MONTH, day=10),
+        date_type(year=YEAR, month=MONTH, day=11),
+        date_type(year=YEAR, month=MONTH, day=12),
+        date_type(year=YEAR, month=MONTH, day=13),
+        date_type(year=YEAR, month=MONTH, day=14),
+        date_type(year=YEAR, month=MONTH, day=15),
+        date_type(year=YEAR, month=MONTH, day=16),
+        date_type(year=YEAR, month=MONTH, day=17),
+        date_type(year=YEAR, month=MONTH, day=19)
+    ])
+
+    assert test_function(MONTH, YEAR, '5-17', '19-21') == set([
+        date_type(year=YEAR, month=MONTH, day=5),
+        date_type(year=YEAR, month=MONTH, day=6),
+        date_type(year=YEAR, month=MONTH, day=7),
+        date_type(year=YEAR, month=MONTH, day=8),
+        date_type(year=YEAR, month=MONTH, day=9),
+        date_type(year=YEAR, month=MONTH, day=10),
+        date_type(year=YEAR, month=MONTH, day=11),
+        date_type(year=YEAR, month=MONTH, day=12),
+        date_type(year=YEAR, month=MONTH, day=13),
+        date_type(year=YEAR, month=MONTH, day=14),
+        date_type(year=YEAR, month=MONTH, day=15),
+        date_type(year=YEAR, month=MONTH, day=16),
+        date_type(year=YEAR, month=MONTH, day=17),
+        date_type(year=YEAR, month=MONTH, day=19),
+        date_type(year=YEAR, month=MONTH, day=20),
+        date_type(year=YEAR, month=MONTH, day=21)
+    ])
+
+    assert test_function(MONTH, YEAR, '5-17', '19-21', '23') == set([
+        date_type(year=YEAR, month=MONTH, day=5),
+        date_type(year=YEAR, month=MONTH, day=6),
+        date_type(year=YEAR, month=MONTH, day=7),
+        date_type(year=YEAR, month=MONTH, day=8),
+        date_type(year=YEAR, month=MONTH, day=9),
+        date_type(year=YEAR, month=MONTH, day=10),
+        date_type(year=YEAR, month=MONTH, day=11),
+        date_type(year=YEAR, month=MONTH, day=12),
+        date_type(year=YEAR, month=MONTH, day=13),
+        date_type(year=YEAR, month=MONTH, day=14),
+        date_type(year=YEAR, month=MONTH, day=15),
+        date_type(year=YEAR, month=MONTH, day=16),
+        date_type(year=YEAR, month=MONTH, day=17),
+        date_type(year=YEAR, month=MONTH, day=19),
+        date_type(year=YEAR, month=MONTH, day=20),
+        date_type(year=YEAR, month=MONTH, day=21),
+        date_type(year=YEAR, month=MONTH, day=23)
+    ])
+
+    assert test_function(MONTH, YEAR, 'mondays') == set([
+        date_type(year=YEAR, month=MONTH, day=1),
+        date_type(year=YEAR, month=MONTH, day=8),
+        date_type(year=YEAR, month=MONTH, day=15),
+        date_type(year=YEAR, month=MONTH, day=22),
+        date_type(year=YEAR, month=MONTH, day=29)
+    ])
+
+    assert test_function(MONTH, YEAR, 'mondays', 'wednesdays') == set([
+        date_type(year=YEAR, month=MONTH, day=1),
+        date_type(year=YEAR, month=MONTH, day=8),
+        date_type(year=YEAR, month=MONTH, day=15),
+        date_type(year=YEAR, month=MONTH, day=22),
+        date_type(year=YEAR, month=MONTH, day=29),
+        date_type(year=YEAR, month=MONTH, day=3),
+        date_type(year=YEAR, month=MONTH, day=10),
+        date_type(year=YEAR, month=MONTH, day=17),
+        date_type(year=YEAR, month=MONTH, day=24),
+        date_type(year=YEAR, month=MONTH, day=31)
+    ])
+
+    assert test_function(MONTH, YEAR, '2-monday') == set([
+        date_type(year=YEAR, month=MONTH, day=8)
+    ])
+
+    assert test_function(MONTH, YEAR, '2-monday', '3-wednesday') == set([
+        date_type(year=YEAR, month=MONTH, day=8),
+        date_type(year=YEAR, month=MONTH, day=17)
+    ])
